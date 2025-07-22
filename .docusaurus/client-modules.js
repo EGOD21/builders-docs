c53713ba4d33302b0143a660d4f4e1bf8bf81e10
EGOD21 <140920088+EGOD21@users.noreply.github.com>
2025-07-22 14:49:18 -0500

    Deploy website - based on b24de4b3824e428b77daa28dd6a591e9c5dba787

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/elliottgodwin/Desktop/builders-docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/elliottgodwin/Desktop/builders-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/elliottgodwin/Desktop/builders-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/elliottgodwin/Desktop/builders-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/elliottgodwin/Desktop/builders-docs/src/css/custom.css"),
-];
