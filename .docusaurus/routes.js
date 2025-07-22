31339ae85476f6501d0cdef9a99d7f9bff183343
EGOD21 <140920088+EGOD21@users.noreply.github.com>
2025-07-22 14:32:07 -0500

    Deploy Docusaurus to GitHub Pages

---
+++ .docusaurus/routes.js
@@ -0,0 +1,225 @@
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+
+export default [
+  {
+    path: '/builders-docs/',
+    component: ComponentCreator('/builders-docs/', '462'),
+    routes: [
+      {
+        path: '/builders-docs/',
+        component: ComponentCreator('/builders-docs/', '0c7'),
+        routes: [
+          {
+            path: '/builders-docs/',
+            component: ComponentCreator('/builders-docs/', 'a07'),
+            routes: [
+              {
+                path: '/builders-docs/BFA/architecture',
+                component: ComponentCreator('/builders-docs/BFA/architecture', 'a6e'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/BFA/next-steps',
+                component: ComponentCreator('/builders-docs/BFA/next-steps', '5bd'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/BFA/overview',
+                component: ComponentCreator('/builders-docs/BFA/overview', '674'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/BFA/repo-link',
+                component: ComponentCreator('/builders-docs/BFA/repo-link', '110'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/brs-inbox-scanner/architecture',
+                component: ComponentCreator('/builders-docs/brs-inbox-scanner/architecture', '0b6'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/brs-inbox-scanner/next-steps',
+                component: ComponentCreator('/builders-docs/brs-inbox-scanner/next-steps', 'ec7'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/brs-inbox-scanner/overview',
+                component: ComponentCreator('/builders-docs/brs-inbox-scanner/overview', '55e'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/brs-inbox-scanner/repo-link',
+                component: ComponentCreator('/builders-docs/brs-inbox-scanner/repo-link', '327'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/BRS-RSA/architecture',
+                component: ComponentCreator('/builders-docs/BRS-RSA/architecture', 'fe9'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/BRS-RSA/next-steps',
+                component: ComponentCreator('/builders-docs/BRS-RSA/next-steps', 'a77'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/BRS-RSA/overview',
+                component: ComponentCreator('/builders-docs/BRS-RSA/overview', '123'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/BRS-RSA/repo-link',
+                component: ComponentCreator('/builders-docs/BRS-RSA/repo-link', 'fd5'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/DBC/architecture',
+                component: ComponentCreator('/builders-docs/DBC/architecture', 'cb2'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/DBC/next-steps',
+                component: ComponentCreator('/builders-docs/DBC/next-steps', '701'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/DBC/overview',
+                component: ComponentCreator('/builders-docs/DBC/overview', '185'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/DBC/repo-link',
+                component: ComponentCreator('/builders-docs/DBC/repo-link', 'edf'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/FoundryOne/architecture',
+                component: ComponentCreator('/builders-docs/FoundryOne/architecture', '464'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/FoundryOne/next-steps',
+                component: ComponentCreator('/builders-docs/FoundryOne/next-steps', '5db'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/FoundryOne/overview',
+                component: ComponentCreator('/builders-docs/FoundryOne/overview', 'b58'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/FoundryOne/repo-link',
+                component: ComponentCreator('/builders-docs/FoundryOne/repo-link', '6df'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/FPD-Sage-Sync/architecture',
+                component: ComponentCreator('/builders-docs/FPD-Sage-Sync/architecture', '045'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/FPD-Sage-Sync/next-steps',
+                component: ComponentCreator('/builders-docs/FPD-Sage-Sync/next-steps', '170'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/FPD-Sage-Sync/overview',
+                component: ComponentCreator('/builders-docs/FPD-Sage-Sync/overview', '83f'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/FPD-Sage-Sync/repo-link',
+                component: ComponentCreator('/builders-docs/FPD-Sage-Sync/repo-link', 'a06'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/Num_Spoofer/architecture',
+                component: ComponentCreator('/builders-docs/Num_Spoofer/architecture', '4e6'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/Num_Spoofer/next-steps',
+                component: ComponentCreator('/builders-docs/Num_Spoofer/next-steps', '3ed'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/Num_Spoofer/overview',
+                component: ComponentCreator('/builders-docs/Num_Spoofer/overview', 'c6a'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/Num_Spoofer/repo-link',
+                component: ComponentCreator('/builders-docs/Num_Spoofer/repo-link', '433'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/ProspectIQ/architecture',
+                component: ComponentCreator('/builders-docs/ProspectIQ/architecture', '2ee'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/ProspectIQ/next-steps',
+                component: ComponentCreator('/builders-docs/ProspectIQ/next-steps', 'f56'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/ProspectIQ/overview',
+                component: ComponentCreator('/builders-docs/ProspectIQ/overview', '5d0'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/ProspectIQ/repo-link',
+                component: ComponentCreator('/builders-docs/ProspectIQ/repo-link', '98c'),
+                exact: true,
+                sidebar: "mainSidebar"
+              },
+              {
+                path: '/builders-docs/',
+                component: ComponentCreator('/builders-docs/', '8c8'),
+                exact: true,
+                sidebar: "mainSidebar"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '*',
+    component: ComponentCreator('*'),
+  },
+];
---
+++ build/assets/js/aba21aa0.f9cfe370.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbuilders_docs=self.webpackChunkbuilders_docs||[]).push([[742],{7093:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
